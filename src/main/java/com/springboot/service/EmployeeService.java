101400f6c4eb888957a07faa94334e0dca3ba3fb
sandeepkokane <kokane.sandeep97@gmail.com>
2020-08-19 09:32:28 +0530

    final src commit

---
+++ src/main/java/com/springboot/service/EmployeeService.java
@@ -0,0 +1,40 @@
+package com.springboot.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springboot.entity.Employee;
+import com.springboot.repository.EmployeeRepository;
+
+@Service
+public class EmployeeService {
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	public List<Employee> getAll() {
+		List<Employee> emp = new ArrayList<>();
+		employeeRepository.findAll().forEach(emp::add);
+		return emp;
+	}
+
+	public Optional<Employee> getEmp(int id) {
+		return employeeRepository.findById(id);
+	}
+
+	public void addEmp(Employee employee) {
+		employeeRepository.save(employee);
+	}
+
+	public Employee updateEmp(Employee employee, int id) {
+		return employeeRepository.save(employee);
+	}
+
+	public void deleteEmp(int id) {
+		employeeRepository.deleteById(id);
+	}
+}
